f7722dcfc12beb2d95f7ef18457fc419a7b71c98
unoseistres <sharondelacruz@gmail.com>
2015-04-06 22:33:20 -0400

    GIFS
    aded beginning animation, prompts, still have to update the tools

--- platforms/android/ant-build/CordovaApp.ap_.d
+++ platforms/android/ant-build/CordovaApp.ap_.d
@@ -61,6 +61,7 @@
 /Users/dickweed/documents/app_7/platforms/android/assets/www/css/images/ui-icons_222222_256x240.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/clear.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/connect.png \
+/Users/dickweed/documents/app_7/platforms/android/assets/www/img/crayon.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/css-images-ui-icons_222222_256x240.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/dibujatucasa.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/download.png \
@@ -72,8 +73,10 @@
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/happyface4.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/happyface5.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/home.png \
+/Users/dickweed/documents/app_7/platforms/android/assets/www/img/juliana_1.gif \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/logo.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/marker.png \
+/Users/dickweed/documents/app_7/platforms/android/assets/www/img/pedro_sketch.gif \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/pedro_sketch.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/pias_text.png \
 /Users/dickweed/documents/app_7/platforms/android/assets/www/img/save.png \
