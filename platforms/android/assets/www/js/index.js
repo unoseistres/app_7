eb9fc5131ff6407ba8c52075e9d587e2b627911e
unoseistres <sharondelacruz@gmail.com>
2015-04-09 03:24:57 -0400

    hidden
    i created a div that only shows up to append the image and hides when
    you draw to canvas, revealing the canvas

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -343,33 +343,19 @@ $(document).one('pagebeforecreate', function () {
   
 
   $("#zoomwrapper1").on("tap",function(){
- //    $("#zoomwrapper1").removeAttr("#zoom1");
- // $("#zoomwrapper1").clone().attr("id",'b1c'+1).appendTo("#mypanel");
-
 
  $("#zoomwrapper1").clone().attr("id",'b1c'+1).appendTo("#mypanel");
  $("#zoom1").removeAttr('id');
  
-
- 
-
- 
-
-      $("#b1c1" ).css('position', 'absolute');
+      $("#b1c1" ).css('position', 'inherit');
       $("#b1c1" ).css('top','70px');
       $("#b1c1" ).css('right', '200px');
       $("#b1c1" ).css('z-index', '99999');
       console.log("cloned");
 
 
-      // var c=document.getElementById("can");
-      // var ctx=c.getContext("2d");
-      // var img=document.getElementById('house');
-      // ctx.drawImage(img,x,y,320,350);
 
-
-
-var hammertime = Hammer(document.getElementById('b1c1'), {
+var hammertime = Hammer(document.getElementById('zoom1'), {
         transform_always_block: true,
          transform_min_scale: 1,
          drag_block_horizontal: true,
@@ -384,9 +370,9 @@ var hammertime = Hammer(document.getElementById('b1c1'), {
          rotation= 0, last_rotation, dragReady=0;
  
      hammertime.on('touch drag dragend transform', function(ev) {
-         elemRect = document.getElementById('zoom1');
+         elemRect = document.getElementById('b1c1');
      manageMultitouch(ev);
-     ev.preventDefault();
+     // ev.preventDefault();
 });
 
 function manageMultitouch(ev){
@@ -427,72 +413,18 @@ case 'touch':
          ev.preventDefault();
    }
 
+$("#b1c1").on("tap",function(){
+  var c=document.getElementById("can");
+    var ctx=c.getContext("2d");
+    var img=document.getElementById('house');
+    ctx.drawImage(img,x,y,320,350);
+    $( "#b1c1" ).remove();
 
+});
 
 
 });
 
- 
-
-            /////////////////////////////////////////////////////////
-
-// var hammertime = Hammer(document.getElementById('zoomwrapper1'), {
-//         transform_always_block: true,
-//         transform_min_scale: 1,
-//         drag_block_horizontal: true,
-//         drag_block_vertical: true,
-//         drag_min_distance: 0
-//     });
- 
-//     var posX=0, posY=0,
-//     lastPosX=0, lastPosY=0,
-//     bufferX=0, bufferY=0,
-//         scale=1, last_scale,
-//         rotation= 0, last_rotation, dragReady=0;
- 
-//     hammertime.on('touch drag dragend transform', function(ev) {
-//         elemRect = document.getElementById('zoom1');
-//     manageMultitouch(ev);
-//     ev.preventDefault();
-//     });
-
-//     function manageMultitouch(ev){
- 
-//     switch(ev.type) {
-//             case 'touch':
-//                 last_scale = scale;
-//                 last_rotation = rotation;
- 
-//                 break;
- 
-//             case 'drag':
-//                   posX = ev.gesture.deltaX + lastPosX;
-//                   posY = ev.gesture.deltaY + lastPosY;
-//                 break;
- 
-//             case 'transform':
-//                 rotation = last_rotation + ev.gesture.rotation;
-//                 scale = Math.max(1, Math.min(last_scale * ev.gesture.scale, 10));
-//                 break;
- 
-//       case 'dragend':
-//         lastPosX = posX;
-//         lastPosY = posY;
-//         break;
-//         }
- 
-//         var transform =
-//                 "translate3d("+posX+"px,"+posY+"px, 0) " +
-//                 "scale3d("+scale+","+scale+", 1) " +
-//                 "rotate("+rotation+"deg) ";
- 
-//         elemRect.style.transform = transform;
-//         elemRect.style.oTransform = transform;
-//         elemRect.style.msTransform = transform;
-//         elemRect.style.mozTransform = transform;
-//         elemRect.style.webkitTransform = transform;
-//         ev.preventDefault();
-//   }
   
    
 });
--- www/js/index.js
+++ www/js/index.js
@@ -342,30 +342,18 @@ $(document).one('pagebeforecreate', function () {
   var y="100";
   
 
-  $("#zoomwrapper1").on("tap",function(){
- //    $("#zoomwrapper1").removeAttr("#zoom1");
- // $("#zoomwrapper1").clone().attr("id",'b1c'+1).appendTo("#mypanel");
+$("#zoomwrapper1").on("tap",function(){
+  $( "#drop" ).show();
 
-
- $("#zoomwrapper1").clone().attr("id",'b1c'+1).appendTo("#mypanel");
+ $("#zoomwrapper1").clone().attr("id",'b1c'+1).appendTo("#drop");
  $("#zoom1").removeAttr('id');
+ $("#zoomwrapper1").removeClass("clone");
  
-
- 
-
- 
-
-      $("#b1c1" ).css('position', 'absolute');
+      $("#b1c1" ).css('position', 'inherit');
       $("#b1c1" ).css('top','70px');
       $("#b1c1" ).css('right', '200px');
       $("#b1c1" ).css('z-index', '99999');
       console.log("cloned");
-
-
-      // var c=document.getElementById("can");
-      // var ctx=c.getContext("2d");
-      // var img=document.getElementById('house');
-      // ctx.drawImage(img,x,y,320,350);
 
 
 
@@ -386,7 +374,7 @@ var hammertime = Hammer(document.getElementById('b1c1'), {
      hammertime.on('touch drag dragend transform', function(ev) {
          elemRect = document.getElementById('zoom1');
      manageMultitouch(ev);
-     ev.preventDefault();
+     // ev.preventDefault();
 });
 
 function manageMultitouch(ev){
@@ -427,72 +415,25 @@ case 'touch':
          ev.preventDefault();
    }
 
+var width= 100;
+var height= 100;
+var x= lastPosX;
+var y = lastPosY;
 
+$("#b1c1").on("tap",function(){
+  var c=document.getElementById("can");
+    var ctx=c.getContext("2d");
+    var img=document.getElementById('house');
+    ctx.drawImage(img,x,y,width,height);
+    $( "#b1c1" ).remove();
+    $("#drop").css("visibility", "hidden");
 
 
 });
 
- 
 
-            /////////////////////////////////////////////////////////
+});
 
-// var hammertime = Hammer(document.getElementById('zoomwrapper1'), {
-//         transform_always_block: true,
-//         transform_min_scale: 1,
-//         drag_block_horizontal: true,
-//         drag_block_vertical: true,
-//         drag_min_distance: 0
-//     });
- 
-//     var posX=0, posY=0,
-//     lastPosX=0, lastPosY=0,
-//     bufferX=0, bufferY=0,
-//         scale=1, last_scale,
-//         rotation= 0, last_rotation, dragReady=0;
- 
-//     hammertime.on('touch drag dragend transform', function(ev) {
-//         elemRect = document.getElementById('zoom1');
-//     manageMultitouch(ev);
-//     ev.preventDefault();
-//     });
-
-//     function manageMultitouch(ev){
- 
-//     switch(ev.type) {
-//             case 'touch':
-//                 last_scale = scale;
-//                 last_rotation = rotation;
- 
-//                 break;
- 
-//             case 'drag':
-//                   posX = ev.gesture.deltaX + lastPosX;
-//                   posY = ev.gesture.deltaY + lastPosY;
-//                 break;
- 
-//             case 'transform':
-//                 rotation = last_rotation + ev.gesture.rotation;
-//                 scale = Math.max(1, Math.min(last_scale * ev.gesture.scale, 10));
-//                 break;
- 
-//       case 'dragend':
-//         lastPosX = posX;
-//         lastPosY = posY;
-//         break;
-//         }
- 
-//         var transform =
-//                 "translate3d("+posX+"px,"+posY+"px, 0) " +
-//                 "scale3d("+scale+","+scale+", 1) " +
-//                 "rotate("+rotation+"deg) ";
- 
-//         elemRect.style.transform = transform;
-//         elemRect.style.oTransform = transform;
-//         elemRect.style.msTransform = transform;
-//         elemRect.style.mozTransform = transform;
-//         elemRect.style.webkitTransform = transform;
-//         ev.preventDefault();
-//   }
   
    
 });
